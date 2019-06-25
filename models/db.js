4ab8d59423b36e2ea76200ea0e654c3ab72e33bd
Ravi Krishnan Iyer <ravikrishaniyer@ccs.neu.edu>
2019-06-25 17:46:59 -0400

    mongo

--- models/db.js
+++ models/db.js
@@ -1,2 +1,2 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/wbdv-su1-19");
+mongoose.connect("mongodb://heroku_zdwwrx3h:87g2hkeelpv8jgth99mujpcj83@ds243607.mlab.com:43607/heroku_zdwwrx3h");
