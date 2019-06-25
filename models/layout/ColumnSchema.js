80571191c98013cddb43609c22c9cc7fdaf4cced
Ravi Krishnan Iyer <ravikrishaniyer@ccs.neu.edu>
2019-06-24 23:28:57 -0400

    created column model

--- models/layout/ColumnSchema.js
+++ models/layout/ColumnSchema.js
@@ -3,6 +3,7 @@ const WidgetSchema = require('./WidgetSchema');
 const ColumnSchema = mongoose.Schema({
     _id: String,
     title: String,
+    span: Number,
     widgets: [WidgetSchema]
 });
 module.exports = ColumnSchema;
